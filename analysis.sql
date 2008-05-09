4ccf138ba47458f110a7452d275fb6072fd2af28
Mark Reid <mark@threewordslong.com>
2008-05-09 15:35:29 +1000

    Updates

--- analysis.sql
+++ analysis.sql
@@ -4,16 +4,16 @@ select
 from
 	pop_pair_all
 order by 
-	Prob;
-	
-select 
-	Count1, Count2, BothCount,
-	work1.Title, work2.Title
-from 
-	pop_pair_all as pair
-	inner join tbllitwork as work1 on work1.LitWorkID = pair.WorkID1
-	inner join tbllitwork as work2 on work2.LitWorkID = pair.WorkID2
-where
-	Count1 >= 100 and Count2 >= 100
-	and BothCount < 5
-;
+	Prob 
+
+-- select 
+-- 	Count1, Count2, BothCount,
+-- 	work1.Title, work2.Title
+-- from 
+-- 	pop_pair_all as pair
+-- 	inner join tbllitwork as work1 on work1.LitWorkID = pair.WorkID1
+-- 	inner join tbllitwork as work2 on work2.LitWorkID = pair.WorkID2
+-- where
+-- 	Count1 >= 100 and Count2 >= 100
+-- 	and BothCount < 5
+-- ;
